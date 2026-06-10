aa6636d15a3fc8865e172b021e0769a42309d781
Croker0 <vitalik.kislov.07@mail.ru>
2026-06-11 00:57:39 +0500

    Девятая практическая

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnCopy = new System.Windows.Forms.Button();
+            this.btnPaste = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.SuspendLayout();
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(39, 87);
+            this.txtInput.Multiline = true;
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(188, 55);
+            this.txtInput.TabIndex = 0;
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Location = new System.Drawing.Point(430, 87);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.ReadOnly = true;
+            this.txtOutput.Size = new System.Drawing.Size(204, 55);
+            this.txtOutput.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(34, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(155, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Введите текст";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(425, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(180, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Текст из буфера";
+            // 
+            // btnCopy
+            // 
+            this.btnCopy.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnCopy.Location = new System.Drawing.Point(39, 224);
+            this.btnCopy.Name = "btnCopy";
+            this.btnCopy.Size = new System.Drawing.Size(115, 42);
+            this.btnCopy.TabIndex = 4;
+            this.btnCopy.Text = "Скопировать в буфер";
+            this.btnCopy.UseVisualStyleBackColor = true;
+            this.btnCopy.Click += new System.EventHandler(this.btnCopy_Click);
+            // 
+            // btnPaste
+            // 
+            this.btnPaste.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnPaste.Location = new System.Drawing.Point(273, 224);
+            this.btnPaste.Name = "btnPaste";
+            this.btnPaste.Size = new System.Drawing.Size(115, 42);
+            this.btnPaste.TabIndex = 5;
+            this.btnPaste.Text = "Вставить из буфера";
+            this.btnPaste.UseVisualStyleBackColor = true;
+            this.btnPaste.Click += new System.EventHandler(this.btnPaste_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnSave.Location = new System.Drawing.Point(505, 224);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(115, 42);
+            this.btnSave.TabIndex = 6;
+            this.btnSave.Text = "Сохранить в файл";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.Filter = "Текстовые файлы (*.txt)|*.txt|Все файлы (*.*)|*.*";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnPaste);
+            this.Controls.Add(this.btnCopy);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.txtInput);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnCopy;
+        private System.Windows.Forms.Button btnPaste;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCopy_Click(object sender, EventArgs e)
+        {
+            // Проверяем, не пустое ли поле
+            if (string.IsNullOrWhiteSpace(txtInput.Text))
+            {
+                MessageBox.Show("Введите текст для копирования!", "Внимание");
+                return;
+            }
+
+            // Метод из методички: Clipboard.SetText
+            Clipboard.SetText(txtInput.Text, TextDataFormat.Text);
+
+            MessageBox.Show("Текст успешно скопирован в буфер обмена!", "Успех");
+        }
+
+        // 2. Кнопка: Вывести любой текст из буфера обмена
+        private void btnPaste_Click(object sender, EventArgs e)
+        {
+            // Метод из методички: Clipboard.GetDataObject()
+            IDataObject clipboardData = Clipboard.GetDataObject();
+
+            // Проверяем, является ли содержимое буфера строкой (текстом)
+            if (clipboardData.GetDataPresent(DataFormats.Text))
+            {
+                // Извлекаем текст и выводим в поле
+                txtOutput.Text = (String)clipboardData.GetData(DataFormats.Text);
+            }
+            else
+            {
+                // Если в буфере не текст (например, картинка или ничего)
+                txtOutput.Text = "Формат данных не поддерживается или буфер пуст";
+            }
+        }
+
+        // 3. Кнопка: Вставить текст из буфера обмена в сторонний файл
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            // Сначала получаем текст из буфера (используем метод GetText из методички)
+            string data = Clipboard.GetText(TextDataFormat.Text);
+
+            if (string.IsNullOrWhiteSpace(data))
+            {
+                MessageBox.Show("Буфер обмена пуст или не содержит текст!", "Внимание");
+                return;
+            }
+
+            // Открываем диалог сохранения файла
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                // Записываем данные в выбранный файл
+                File.WriteAllText(saveFileDialog1.FileName, data);
+
+                txtOutput.Text = data; // Дублируем в поле для наглядности
+                MessageBox.Show($"Текст успешно сохранен в файл:\n{saveFileDialog1.FileName}", "Успех");
+            }
+        }
+    }
+}
